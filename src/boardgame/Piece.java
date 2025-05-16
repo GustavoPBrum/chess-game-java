c76906c00e0fe24e05dc7493d9c80bc62dccf3ab
Gustavo Pereira Brum <brumtavo@gmail.com>
2025-05-16 01:26:47 +0100

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+public class Piece {
+	
+	// Protected pois queremos que essa posicao seja da matriz de tabuleiro
+	protected Position position;
+	
+	private Board board;
+
+	public Piece(Board board) {
+		super();
+		this.board = board;
+	}
+
+	protected Board getBoard() {  
+		return board;
+	}
+	
+	
+}
