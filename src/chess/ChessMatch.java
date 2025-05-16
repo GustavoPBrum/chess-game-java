49fe37f3723290730eebcbcaaac2591e29a76d2a
Gustavo Pereira Brum <brumtavo@gmail.com>
2025-05-16 04:12:27 +0100

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -15,9 +15,9 @@ public class ChessMatch { // Coracao do sistema de xadrez, onde ficara as regras
 	}
 	
 	public ChessPiece[][] getPieces() {
-		ChessPiece[][] mat = new ChessPiece[board.getrow()][board.getColumn()];
-		for(int i = 0; i < board.getrow(); i++) {
-			for(int j = 0; j < board.getColumn(); j++) {
+		ChessPiece[][] mat = new ChessPiece[board.getrows()][board.getcolumns()];
+		for(int i = 0; i < board.getrows(); i++) {
+			for(int j = 0; j < board.getcolumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);  //  Recebe cada peca do tabuleiro e faz o downcasting para peca de xadrez
 			}
 		}
